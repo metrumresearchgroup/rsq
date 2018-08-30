607fccd5d9bebd02fc56dd975a47488cf9da1b98
Devin Pastoor <devin.pastoor@gmail.com>
2018-08-30 10:35:51 -0400

    init with modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dpastoor/srq
+
+require github.com/dpastoor/goutils v1.2.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dpastoor/goutils"
+)
+
+func main() {
+	fmt.Println(goutils.LeftPad("1", "0", 2))
+}
