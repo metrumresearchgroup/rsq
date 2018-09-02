e0eb2e9180b07ea7ef7715c336b054c309307ca9
Devin Pastoor <devin.pastoor@gmail.com>
2018-09-02 09:15:20 -0400

    add cli

---
+++ cmd/rsq/rsq.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/metrumresearchgroup/pkgcheck/cmd"
+)
+
+// if want to generate docs
+//	import "github.com/spf13/cobra/doc"
+//	err := doc.GenMarkdownTree(cmd.RootCmd, "../../docs/bbi")
+//	if err != nil {
+//		panic(err)
+//	}
+func main() {
+	cmd.Execute()
+}
