8c0e72fde7f94245098671190f0a4346b27f7908
Devin Pastoor <devin.pastoor@gmail.com>
2018-09-02 10:42:46 -0400

    add exec settings

--- runner/structs.go
+++ runner/structs.go
@@ -1,5 +1,11 @@
 package runner
 
+// ExecSettings controls settings related to Rscript execution
+type ExecSettings struct {
+	WorkDir string
+	Rfile   string
+}
+
 // RSettings controls settings related to managing libraries
 type RSettings struct {
 	LibPaths []string          `json:"lib_paths,omitempty"`
