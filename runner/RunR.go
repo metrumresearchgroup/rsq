209b18cc021d0e572f40851089ab32b5f9b8912b
Devin Pastoor <devin.pastoor@gmail.com>
2018-09-02 10:22:59 -0400

    add running r script

--- runner/RunR.go
+++ runner/RunR.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 
@@ -55,5 +56,56 @@ func RunR(
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
+	return cmd.Run()
+}
+
+// RunR launches an interactive R console
+func RunRscript(
+	fs afero.Fs,
+	rs RSettings,
+	rdir string, // this should be put into RSettings
+	rfile string,
+	lg *logrus.Logger,
+) error {
+
+	cmdArgs := []string{
+		"--no-save",
+		"--no-restore-data",
+		rfile,
+	}
+
+	envVars := os.Environ()
+	ok, rLibsSite := rs.LibPathsEnv()
+	if ok {
+		envVars = append(envVars, rLibsSite, "R_LIBS=''")
+	}
+
+	lg.WithFields(
+		logrus.Fields{
+			"cmdArgs":   cmdArgs,
+			"RSettings": rs,
+			"env":       rLibsSite,
+		}).Debug("command args")
+
+	// --vanilla is a command for R and should be specified before the CMD, eg
+	// R --vanilla CMD check
+	// if cs.Vanilla {
+	// 	cmdArgs = append([]string{"--vanilla"}, cmdArgs...)
+	// }
+	cmd := exec.Command(
+		fmt.Sprintf("%sscript", rs.R()),
+		cmdArgs...,
+	)
+
+	if rdir == "" {
+		rdir, _ = os.Getwd()
+		lg.WithFields(
+			logrus.Fields{"rdir": rdir},
+		).Debug("launch dir")
+	}
+	cmd.Dir = rdir
+	cmd.Env = envVars
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
