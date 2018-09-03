b0540b456979580ad14e1d72d2ad864e240dce9b
Devin Pastoor <devin.pastoor@gmail.com>
2018-09-03 09:19:30 -0400

    should store times as UTC

--- server/db/internal/internal_test.go
+++ server/db/internal/internal_test.go
@@ -16,9 +16,9 @@ func TestMarshalJob(t *testing.T) {
 		ID:     u1.String(),
 		Status: "COMPLETED",
 		RunDetails: server.RunDetails{
-			QueueTime: time.Now().AddDate(0, 0, -1),
-			StartTime: time.Now().AddDate(0, 0, 0),
-			EndTime:   time.Now().AddDate(0, 0, 1),
+			QueueTime: time.Now().AddDate(0, 0, -1).UTC(),
+			StartTime: time.Now().AddDate(0, 0, 0).UTC(),
+			EndTime:   time.Now().AddDate(0, 0, 1).UTC(),
 			Error:     "no error",
 		},
 	}
@@ -29,6 +29,9 @@ func TestMarshalJob(t *testing.T) {
 	} else if err := UnmarshalJob(buf, &result); err != nil {
 		t.Fatal(err)
 	} else if !reflect.DeepEqual(testJob, result) {
+		// t.Log("output")
+		// t.Logf("%s", testJob)
+		// t.Logf("%s", result)
 		t.Fatalf("unexpected copy: %#v", result)
 	}
 }
