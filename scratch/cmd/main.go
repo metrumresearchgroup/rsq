767f1ae644ef7612673635c7ac071c3f6adb4119
Devin Pastoor <devin.pastoor@gmail.com>
2018-09-03 15:03:32 -0400

    start saving some scratch functions

--- scratch/cmd/main.go
+++ scratch/cmd/main.go
@@ -13,6 +13,11 @@ func main() {
 	appFS := afero.NewOsFs()
 	lg := logrus.New()
 	lg.SetLevel(logrus.DebugLevel)
+}
+
+// runScriptExample(appFS, lg, "add.R")
+// runScriptExample(appFS, lg, "add-stop.R")
+func runScriptExample(appFS afero.Fs, lg *logrus.Logger, rfile string) {
 	runSettings := runner.RSettings{
 		LibPaths: []string{},
 		Rpath:    "R",
@@ -20,7 +25,7 @@ func main() {
 	wd, _ := os.Getwd()
 	execSettings := runner.ExecSettings{
 		WorkDir: wd,
-		Rfile:   "add-stop.R",
+		Rfile:   rfile,
 	}
 	// if passes will give exit code 0
 	// if stopped will give exit code 1
